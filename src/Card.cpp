520807b306102e9bb2d8f7bbdffc91c9ce1c00fa
pstalcup <legomaniac0117@gmail.com>
2016-02-07 10:54:20 -0500

    Warnings as errors, render a "hand of cards"

---
+++ src/Card.cpp
@@ -0,0 +1,5 @@
+//
+// Created by patrick on 2/7/16.
+//
+
+#include "Card.h"
---
+++ src/Card.h
@@ -0,0 +1,15 @@
+//
+// Created by patrick on 2/7/16.
+//
+
+#ifndef SDL_GAME_CARD_H
+#define SDL_GAME_CARD_H
+
+
+class Card
+{
+
+};
+
+
+#endif //SDL_GAME_CARD_H
